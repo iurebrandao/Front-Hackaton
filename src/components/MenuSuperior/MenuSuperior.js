91b4cf9192cf267eedb0a76c2e1abb65fcafbf55
Gabriel Pereira Pinheiro <gabriel.pereira.pinheiro@gmail.com>
2020-06-27 17:26:57 -0300

    feat barra superior pagina inicial

---
+++ src/components/MenuSuperior/MenuSuperior.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import classes from './MenuSuperior.css';
+import unb_logo from "../../assets/img/unb_logo.svg"
+
+class MenuSuperior extends Component {
+
+    routeTo = (destiny) => {
+        this.props.history.push({
+            pathname: '/' + destiny,
+        });
+    }
+
+    render() {
+        return (
+            <div className="barraSuperior">
+                <img className="logoUnbBarra" alt="error" src={unb_logo} />
+            </div>
+        );
+    }
+}
+
+export default MenuSuperior;
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 // import { NavLink } from 'react-router-dom';
+import MenuSuperior from "../../components/MenuSuperior/MenuSuperior";
 
 class HomePage extends Component {
 
@@ -12,29 +13,7 @@ class HomePage extends Component {
     render() {
         return (
             <div class="div-home-page full-page">
-
-
-                <div class="div-60">
-
-                    <div class="info">
-                        <h3 onClick={(e) => this.routeTo("")} class="info-text float-right">Início</h3>
-                        <h3 onClick={(e) => this.routeTo("sign-in")} class="info-text float-right">Login</h3>
-                        <h3 onClick={(e) => this.routeTo("sign-up")} class="info-text float-right">Cadastro</h3>
-                        <h3 onClick={(e) => this.routeTo("about")} class="info-text float-right">Sobre</h3>
-                    </div>
-
-                    <div class="main-area">
-
-                        <h1 class="title-home-page title-color">UnB</h1>
-                        <h2 class="sub-title-home-page title-color">Amigo</h2>
-                        <div class="barra-azul"></div>
-
-                        <button onClick={(e) => this.routeTo("sign-in")} id="button-entrar">Entrar</button>
-
-                    </div>
-                </div>
-
-
+                <MenuSuperior></MenuSuperior>
 
             </div>
         );
