a55393560c31ce980e0c96713cc9613a46d2c2c9
iure <iurebrandao@gmail.com>
2020-06-29 22:27:28 -0300

    <feat>: done

--- src/containers/Material/Material.js
+++ src/containers/Material/Material.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Material.css';
 import pdf from "../../assets/img/pdf.svg";
 import share from "../../assets/img/share.svg";
+import MaterialCard from "../../components/MaterialCard/MaterialCard";
 
 const Material = (props) => {
     return(
@@ -9,180 +10,30 @@ const Material = (props) => {
             <h2>Material Didático</h2>
             <div className="app-block">
                 <h3 className="app-block__title">Engenharia de Software</h3>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
+                <MaterialCard name="Ficha - ES 1/2020"/>
+                <MaterialCard name="Ficha - FIS 1/2020"/>
+                <MaterialCard name="Ficha - CIC 1/2020"/>
+                <MaterialCard name="Ficha - ES 2/2020"/>
+                <MaterialCard name="Ficha - FIS 2/2020"/>
+                <MaterialCard name="Ficha - CIC 2/2020"/>
             </div>
             <div className="app-block">
-                <h3 className="app-block__title">Engenharia de Software</h3>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
+                <h3 className="app-block__title">Circuitos Elétricos</h3>
+                <MaterialCard name="Ficha - ES 1/2020"/>
+                <MaterialCard name="Ficha - FIS 1/2020"/>
+                <MaterialCard name="Ficha - CIC 1/2020"/>
+                <MaterialCard name="Ficha - ES 2/2020"/>
+                <MaterialCard name="Ficha - FIS 2/2020"/>
+                <MaterialCard name="Ficha - CIC 2/2020"/>
             </div>
             <div className="app-block">
-                <h3 className="app-block__title">Engenharia de Software</h3>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
-                <div className="app-block__item">
-                    <img className="app-block__itemimg" src={pdf} alt="pdf icon" />
-                    <span>Ficha - ES 2/2020</span>
-                    <img className="app-block__itemicon" src={share} alt="share icon" />
-                    <div className="app-block__itemsquaregrey"></div>
-                    <div className="app-block__itemsquaregreen"></div>
-                </div>
+                <h3 className="app-block__title">Física</h3>
+                <MaterialCard name="Ficha - ES 1/2020"/>
+                <MaterialCard name="Ficha - FIS 1/2020"/>
+                <MaterialCard name="Ficha - CIC 1/2020"/>
+                <MaterialCard name="Ficha - ES 2/2020"/>
+                <MaterialCard name="Ficha - FIS 2/2020"/>
+                <MaterialCard name="Ficha - CIC 2/2020"/>
             </div>
         </div>
     )
